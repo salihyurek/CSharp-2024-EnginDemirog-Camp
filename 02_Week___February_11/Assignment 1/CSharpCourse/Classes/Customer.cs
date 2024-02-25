b8247a9f9c15214469cd8647a741765c5aff97cb
Salih Yurek <salihyurek@gmail.com>
2024-02-26 00:05:18 +0300

    Updated Classes File

--- 02_Week___February_11/Assignment 1/CSharpCourse/Classes/Customer.cs
+++ 02_Week___February_11/Assignment 1/CSharpCourse/Classes/Customer.cs
@@ -2,6 +2,10 @@ namespace Classes
 {
     public class Customer
     {
+        // field 
+        // public string FirstName;
+        
+        // Property
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
