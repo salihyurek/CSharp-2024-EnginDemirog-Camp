6ef1e58861443b0dba4b502bddf045a51337a60e
Salih Yurek <salihyurek@gmail.com>
2024-02-26 02:03:40 +0300

    Added Inheritance File

---
+++ 02_Week___February_11/Assignment 1/CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName = "Salih"},
+                new Student{FirstName = "Engin"},
+                new Person{FirstName = "Derin"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    
+    class Customer: Person
+    {
+        public string City { get; set; }
+    }
+    class Student: Person
+    {
+        public string Department { get; set; }
+    }
+}
