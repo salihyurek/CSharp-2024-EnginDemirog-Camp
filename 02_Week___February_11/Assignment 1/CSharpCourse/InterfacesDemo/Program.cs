7aaeae1e31692a0da800586797b74b1a9864e5fd
Salih Yurek <salihyurek@gmail.com>
2024-02-26 01:49:51 +0300

    Added InterfacesDemo File

---
+++ 02_Week___February_11/Assignment 1/CSharpCourse/InterfacesDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Interface Segregation
+            IWorker[] workers = new IWorker[3]
+            {   
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+    
+    class Manager:IWorker, IEat, ISalary
+    {
+        public void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Eat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class Worker:IWorker, IEat, ISalary
+    {
+        public void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Eat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class Robot:IWorker
+    {
+        public void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
