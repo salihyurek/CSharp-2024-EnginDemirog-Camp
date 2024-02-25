d7d8c4d2597678293c3ec12079cbe6822216bd88
Salih Yurek <salihyurek@gmail.com>
2024-02-25 23:51:09 +0300

    Added Arrays File

--- 02_Week___February_11/Assignment 1/CSharpCourse/Arrays/Program.cs
+++ 02_Week___February_11/Assignment 1/CSharpCourse/Arrays/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace Arrays
+﻿using System;
+
+namespace Arrays
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string student1 = "Engin";
+            string student2 = "Derin";
+            string student3 = "Salih";
+
+
+            string[] students = new string [3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+            string[] students2 = { "Engin", "Derin", "Salih" };
+            // students2[3] = "Ahmet"; // error 
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            string[,] regions = new string[7, 3]
+            {
+                // regions[0, 0] = "İstanbul";
+                {"İstanbul","Bursa","Kocaeli"},
+                {"Ankara","Konya","Kayseri"},
+                {"Antalya","Adana","Mersin"},
+                {"Samsun","Trabzon","Ordu"},
+                {"İzmir","Manisa","Aydın"},
+                {"Gaziantep","Diyarbakır","Şanlıurfa"},
+                {"Erzurum","Van","Erzincan"}
+            };
+
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("*****");
+            }
+        
         }
     }
 }
