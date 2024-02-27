03f92798d1bebc9c55ef31bb059dbe84581e14ba
Salih Yurek <salihyurek@gmail.com>
2024-02-27 22:29:41 +0300

    Added Solution: Interfaces

--- 02_Week___February_11/Assignment 2/CSharpIntro/Constructors/Program.cs
+++ 02_Week___February_11/Assignment 2/CSharpIntro/Constructors/Program.cs
@@ -6,7 +6,22 @@ namespace Concructures
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
+            Customer customer1 = new Customer{Id = 1,FirstName = "Salih",LastName = "Yürek",City = "Tokat"};
+
+            Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            
+            
+            Console.WriteLine(customer2.FirstName);
+
+            // Method(1,"sfsfdf","sdfs","sdfsdfs");
+        }
+
+        static void Method(int Id, string firstName, string lastName, string city)
+        {
+            
         }
         
     }
@@ -16,6 +31,16 @@ namespace Concructures
         public Customer()
         {
             
+        }
+        
+        // default Constructor
+        public Customer(int id,string firstName,string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+            // Console.WriteLine("Kurucu blok çalıştı");
         }
         
         public int Id { get; set; }
---
+++ 02_Week___February_11/Assignment 2/CSharpIntro/Interfaces/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Interfaces
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // intefaces new lenemez
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(customerManager);
+        }
+    }
+
+    interface IPersonManager
+    {
+        // unimplemented operation
+        void Add();
+        void Update();
+    }
+    
+    // intheritance, class larda kullanılır. // implements, interface lerde kullanılır 
+    class CustomerManager: IPersonManager
+    {
+        public void Add()
+        {
+            // Müşteri Ekleme Kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager: IPersonManager
+    {
+        public void Add()
+        {
+            // Personel Ekleme Kodları
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
